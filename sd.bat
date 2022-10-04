d1cba1f81dc4012b92dfaf6db7cef7ed22758175
Awashcard0 <95319856+Awashcard0@users.noreply.github.com>
2022-10-04 17:52:46 -0400

    Update sd.bat

--- sd.bat
+++ sd.bat
@@ -1,14 +1,9 @@
 echo off
-echo 5
-timeout 1
-echo 4
-timeout 1
 echo 3
-timeout 1
-echo 2
-timeout 1
-echo 1
-timeout 1
-echo bye
 timeout 1 > nul
+echo 2
+timeout 1 > nul
+echo 1
+timeout 1 > nul
+echo bye
 shutdown /f /p
