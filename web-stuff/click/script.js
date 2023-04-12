ca03c3d32190f411892bcc13d2d6c882cf1564c2
Awashcard0 <95319856+Awashcard0@users.noreply.github.com>
2023-04-12 10:15:12 -0400

    some fix and also home thing

--- web-stuff/click/script.js
+++ web-stuff/click/script.js
@@ -79,7 +79,7 @@ function setCookie() {
 const d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
   let expires = "expires=" + d.toUTCString();
-  document.cookie = e + expires + ";path=/":
+  document.cookie = e + expires + ";path=/";
 }
 
 function save() {
